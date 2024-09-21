fcf5d7cdc87566b6829ffd558630557681938a97
KevinLai02 <jihn.smith629@gmail.com>
2024-09-21 21:57:28 +0800

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,45 @@
-import 'dotenv/config'
-import express, { Request, Response } from 'express'
-import http from 'http'
-import { Server } from 'socket.io'
-import { ClientToServerEvents, ServerToClientEvents } from './types'
-const app = express()
-const server = http.createServer(app)
-const port = process.env.PORT || 3000
+  import 'dotenv/config'
+  import express, { Request, Response } from 'express'
+  import http from 'http'
+  import { Server } from 'socket.io'
+  import { ClientToServerEvents, ServerToClientEvents } from './types'
+  const app = express()
+  const server = http.createServer(app)
+  const port = process.env.PORT || 3000
 
-const io = new Server<
-ClientToServerEvents,
-ServerToClientEvents
->(server)
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
-})
+  const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents
+  >(server)
+  app.get('/', (req:Request, res:Response) => {
+    res.send('Hello World!')
+  })
 
-io.on('connection', (socket) => {
-  console.log('connecting')
-  socket.on('joinRoom', (req) => {
-    const { roomID } = req
-    socket.join(roomID)
-    io.to(roomID).emit('joinRoom', req)
+  io.on('connection', (socket) => {
+    console.log('connecting')
+    socket.on('joinRoom', (req) => {
+      const { roomID } = req
+      socket.join(roomID)
+      io.to(roomID).emit('joinRoom', req)
+    })
+    socket.on('onReady', (req) => {
+      const { roomID, userName } = req
+      io.to(roomID).emit('onReady', { roomID, userName })
+    })
+    socket.on('userJoin', (req) => {
+      const { roomID } = req
+      io.to(roomID).emit('userJoin', req)
+    })
+    socket.on('start', (req) => {
+      const { roomID } = req
+      io.to(roomID).emit('start', { message: 'starting' })
+    })
+    socket.on('mora', (req) => {
+      const { roomID, mora, userName } = req
+      io.to(roomID).emit('mora', { roomID, mora, userName })
+    })
   })
-  socket.on('onReady', (req) => {
-    const { roomID, userName } = req
-    io.to(roomID).emit('onReady', { roomID, userName })
-  })
-  socket.on('userJoin', (req) => {
-    const { roomID } = req
-    io.to(roomID).emit('userJoin', req)
-  })
-  socket.on('start', (req) => {
-    const { roomID } = req
-    io.to(roomID).emit('start', { message: 'starting' })
-  })
-  socket.on('mora', (req) => {
-    const { roomID, mora, userName } = req
-    io.to(roomID).emit('mora', { roomID, mora, userName })
-  })
-})
 
-server.listen(port, () => {
-  console.log(`listening on *:${port}`)
-})
+  server.listen(port, () => {
+    console.log(`listening on *:${port}`)
+  })
